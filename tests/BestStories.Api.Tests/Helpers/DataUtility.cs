93e32bbce06bbd339375970399f5cd56f9f8f748
grantcolley <grantcolley@yahoo.com>
2023-08-04 22:28:29 +0100

    Update DataUtility.cs

--- tests/BestStories.Api.Tests/Helpers/DataUtility.cs
+++ tests/BestStories.Api.Tests/Helpers/DataUtility.cs
@@ -16,21 +16,5 @@ namespace BestStories.Api.Tests.Helpers
             string jsonStories = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "StoriesCache.txt"));
             return JsonSerializer.Deserialize<IEnumerable<Story>>(jsonStories) ?? throw new NullReferenceException(nameof(jsonStories));
         }
-
-        public static IEnumerable<Story> GetUpdatedBestStories()
-        {
-            string jsonStories = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "StoriesCache.txt"));
-            IEnumerable<Story>? stories = JsonSerializer.Deserialize<IEnumerable<Story>>(jsonStories) ?? throw new NullReferenceException(nameof(jsonStories));
-            List<Story> updatedStories = new(stories);
-
-            updatedStories.AddRange(new[]
-            {
-                new Story{ id = 1, score = 1001, title = "New Story 1" },
-                new Story{ id = 2, score = 1002, title = "New Story 2" },
-                new Story{ id = 3, score = 1003, title = "New Story 3" }
-            });
-
-            return updatedStories;
-        }
     }
 }
